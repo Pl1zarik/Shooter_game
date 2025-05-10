7dd90ccaf289e196b4aa0bb6ef5033be8ed82abc
Pl1zarik <121376270+Pl1zarik@users.noreply.github.com>
2025-05-10 09:29:40 +0300

    Add files via upload

--- about.py
+++ about.py
@@ -8,7 +8,7 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (66,170,255)
-screen_flag = True
+
 FPS = 60
 
 # Настройка дисплея
@@ -18,14 +18,13 @@ back = transform.scale(image.load('back_about.png'), (WIDTH, HEIGHT))
 
 
 def about():
-
+    screen_flag = True
     clock = time.Clock()
 
-    while screen_flag!= False:
+    while screen_flag:
         for e in event.get():
             if e.type == QUIT:
-                pygame.quit()
-                sys.exit()
+                screen_flag = False
     
         main_win.blit(back, (0, 0))
         display.flip()
--- screen_menu.py
+++ screen_menu.py
@@ -1,4 +1,4 @@
-from s_game import game, FLAG, finish#, about
+from s_game import game, FLAG, finish  # , about
 from about import about
 from pygame import *
 import pygame
@@ -10,23 +10,29 @@ WIDTH, HEIGHT = 800, 600
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-BLUE = (66,170,255)
+BLUE = (66, 170, 255)
 screen_flag = True
 FPS = 60
 
+
 # класс
-class Object():
+class Object:
     def __init__(self, object_image, object_x, object_y, size_x, size_y):
         self.image = transform.scale(image.load(object_image), (size_x, size_y))
         self.rect = self.image.get_rect()
         self.rect.x = object_x
         self.rect.y = object_y
+
     def reset(self):
         screen.blit(self.image, (self.rect.x, self.rect.y))
 
 
+# Инициализация Pygame
+font.init()
+
+
 def draw_text(text, size, x, y):
-    font = pygame.font.SysFont('Arial', size)
+    font = pygame.font.SysFont("Arial", size)
     text_surface = font.render(text, True, (0, 0, 0))
     text_rect = text_surface.get_rect(center=(x, y))
     screen.blit(text_surface, text_rect)
@@ -34,32 +40,29 @@ def draw_text(text, size, x, y):
 
 # музыка
 mixer.init()
-game_music = mixer.Sound('space.ogg')
-mixer.music.load('menu_music.ogg')
+mixer.music.load("menu_music.ogg")
 mixer.music.set_volume(0.2)
-mixer.music.play()
 # if FLAG == True:
 #     mixer.music.pause()
 # elif FLAG == False:
 #     mixer.music.unpause()
 
-button_play = Object('button_play.png', WIDTH/2 - 70, HEIGHT/2 + 150, 140, 60)
-button_about = Object('button_about.png', WIDTH/2 - 70, HEIGHT/2 + 209, 140, 60)
-back = transform.scale(image.load('back_menu.jpg'), (WIDTH, HEIGHT))
-
-# Инициализация Pygame
-mixer.init()
-font.init()
+button_play = Object("button_play.png", WIDTH / 2 - 70, HEIGHT / 2 + 150, 140, 60)
+button_about = Object("button_about.png", WIDTH / 2 - 70, HEIGHT / 2 + 209, 140, 60)
+back = transform.scale(image.load("back_menu.jpg"), (WIDTH, HEIGHT))
 
 
 # Настройка дисплея
 screen = display.set_mode((WIDTH, HEIGHT))
 display.set_caption("Шутер")
 
+is_play_music = False
 
 clock = time.Clock()
-
-while screen_flag!= False:
+while screen_flag:
+    if not is_play_music:
+        mixer.music.play(-1)
+        is_play_music = True
     for e in event.get():
         if e.type == QUIT:
             pygame.quit()
@@ -69,29 +72,28 @@ while screen_flag!= False:
         if e.type == MOUSEBUTTONDOWN:
             if e.button == 1:
                 click = True
-    
+
     # screen.fill(WHITE)
     screen.blit(back, (0, 0))
-    draw_text("Добро пожаловать!", 40, WIDTH/2, HEIGHT/4)
+    draw_text("Добро пожаловать!", 40, WIDTH / 2, HEIGHT / 4)
     mx, my = mouse.get_pos()
     button_play.reset()
     button_about.reset()
-    button_1 = Rect(WIDTH/2 - 70, HEIGHT/2 + 150, 140, 50)
-    button_2 = Rect(WIDTH/2 - 70, HEIGHT/2 + 150 + 60, 140, 50)
-    draw_text("Play", 30, WIDTH/2, HEIGHT/2 + 175)
-    draw_text("About", 30, WIDTH/2, HEIGHT/2 + 235)
+    button_1 = Rect(WIDTH / 2 - 70, HEIGHT / 2 + 150, 140, 50)
+    button_2 = Rect(WIDTH / 2 - 70, HEIGHT / 2 + 150 + 60, 140, 50)
+    draw_text("Play", 30, WIDTH / 2, HEIGHT / 2 + 175)
+    draw_text("About", 30, WIDTH / 2, HEIGHT / 2 + 235)
 
     if button_1.collidepoint((mx, my)):
-        if click: game() 
+        if click:
+            mixer.music.stop()
+            is_play_music = False
+            game()
         # finish = False
-        # game_music.play()
     if button_2.collidepoint((mx, my)):
-        if click: about()
+        if click:
+            about()
 
-
-    
     click = False
     display.flip()
     clock.tick(FPS)
-
- 
